548d7904c0a18dee0865b2e59325bfa16bbccfb0
notsugat <sugatsujakhu@gmail.com>
2024-12-21 13:44:30 +0545

    fix: navbar routing fix

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Button } from "@/components/ui/button.jsx";
 import { Logo } from "@/components/ui/logo.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <header className="border-b px-[10%] py-6 z-40 shadow-sm bg-white sticky top-0">
       <div className=" flex  items-center justify-between ">
-        <Logo />
+        <button onClick={() => navigate("/")}>
+          <Logo />
+        </button>
         <nav className="hidden md:flex items-center space-x-6 text-blue-600">
           <Button variant="ghost" className="hover:text-blue-800">
             sgRNA Design
