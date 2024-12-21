83dbd3faa48649afa06ead97c1471b53b60eadbf
notashwinii <ashwinii.subedi@gmail.com>
2024-12-21 15:06:48 +0545

    data

---
+++ frontend/src/lib/data.js
@@ -0,0 +1,292 @@
+export const RNAData = [
+  {
+    ID: 1,
+    sgRNASequence: "GGTGAAATGAGGGCTTGCGA",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+42",
+    Chromosome: 6,
+    Location: 31138428,
+    Strand: "-",
+    EScore: 20,
+    SScore: -3,
+    EPlusSScore: 17
+  },
+  {
+    ID: 2,
+    sgRNASequence: "TCGCAAGCCCTCATTTCACC",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+42",
+    Chromosome: 6,
+    Location: 31138428,
+    Strand: "+",
+    EScore: 20,
+    SScore: -4,
+    EPlusSScore: 16
+  },
+  {
+    ID: 3,
+    sgRNASequence: "GTGAAATGAGGGCTTGCGAA",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+41",
+    Chromosome: 6,
+    Location: 31138429,
+    Strand: "-",
+    EScore: 20,
+    SScore: -5,
+    EPlusSScore: 15
+  },
+  {
+    ID: 4,
+    sgRNASequence: "GCCGGGGGCCTGGTGAAATG",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+53",
+    Chromosome: 6,
+    Location: 31138417,
+    Strand: "-",
+    EScore: 20,
+    SScore: -7,
+    EPlusSScore: 13
+  },
+  {
+    ID: 5,
+    sgRNASequence: "AGGAGGCAAGTGAGCTTCGA",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+493",
+    Chromosome: 6,
+    Location: 31137977,
+    Strand: "+",
+    EScore: 15,
+    SScore: -3,
+    EPlusSScore: 12
+  },
+  {
+    ID: 6,
+    sgRNASequence: "ATCCCCCCACAGAACTCATA",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+291",
+    Chromosome: 6,
+    Location: 31138179,
+    Strand: "-",
+    EScore: 20,
+    SScore: -8,
+    EPlusSScore: 12
+  },
+  {
+    ID: 7,
+    sgRNASequence: "GTAGGTTCTTGAATCCCGAA",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1057",
+    Chromosome: 6,
+    Location: 31133890,
+    Strand: "+",
+    EScore: 12,
+    SScore: 0,
+    EPlusSScore: 12
+  },
+  {
+    ID: 8,
+    sgRNASequence: "TGAGTCAGTGAACAGGGAAT",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+942",
+    Chromosome: 6,
+    Location: 31134005,
+    Strand: "+",
+    EScore: 12,
+    SScore: 0,
+    EPlusSScore: 12
+  },
+  {
+    ID: 9,
+    sgRNASequence: "CACAGAACTCATACGGCGGG",
+    TargetGene: "POU5F1",
+    TranscriptID: "NM_002701",
+    DistanceToTSS: "+284",
+    Chromosome: 6,
+    Location: 31138186,
+    Strand: "-",
+    EScore: 20,
+    SScore: -9,
+    EPlusSScore: 11
+  },
+  {
+    ID: 10,
+    sgRNASequence: "CTACTCCTCTTCATGGGTGA",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1351",
+    Chromosome: 6,
+    Location: 31133596,
+    Strand: "-",
+    EScore: 12,
+    SScore: -1,
+    EPlusSScore: 11
+  },
+  {
+    ID: 11,
+    sgRNASequence: "TATTCCTTGGGGCCACACGT",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1039",
+    Chromosome: 6,
+    Location: 31133908,
+    Strand: "+",
+    EScore: 12,
+    SScore: -1,
+    EPlusSScore: 11
+  },
+  {
+    ID: 12,
+    sgRNASequence: "AGAACCTACGTGTGGCCCCA",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1046",
+    Chromosome: 6,
+    Location: 31133901,
+    Strand: "-",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 13,
+    sgRNASequence: "CTGACCGCATCTCCCCTCTA",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1285",
+    Chromosome: 6,
+    Location: 31133662,
+    Strand: "+",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 14,
+    sgRNASequence: "GATGCGGTCAGAATCTGCAG",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1276",
+    Chromosome: 6,
+    Location: 31133671,
+    Strand: "-",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 15,
+    sgRNASequence: "GGAGTATCCCTGAACCTAGT",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1306",
+    Chromosome: 6,
+    Location: 31133641,
+    Strand: "+",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 16,
+    sgRNASequence: "GGTGAATGACATTTGTGGGT",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+963",
+    Chromosome: 6,
+    Location: 31133984,
+    Strand: "+",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 17,
+    sgRNASequence: "GGTTATTTCTAGAAGTTAGG",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+984",
+    Chromosome: 6,
+    Location: 31133963,
+    Strand: "+",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  },
+  {
+    ID: 18,
+    sgRNASequence: "TCTTGAATCCCGAATGGAAA",
+    TargetGene: "POU5F1",
+    TranscriptIDs: [
+      "NM_001173531",
+      "NM_001285986",
+      "NM_001285987",
+      "NM_203289"
+    ],
+    DistanceToTSS: "+1063",
+    Chromosome: 6,
+    Location: 31133884,
+    Strand: "+",
+    EScore: 12,
+    SScore: -2,
+    EPlusSScore: 10
+  }
+];
+
